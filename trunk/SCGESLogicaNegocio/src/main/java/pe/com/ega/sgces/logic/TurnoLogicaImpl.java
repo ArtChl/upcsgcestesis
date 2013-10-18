06bb54218fbcd334542e60b4c8b613bea19f7256
screaber@gmail.com <screaber@gmail.com@681f322d-baa2-ebcc-1b8f-986ddf46640d>
2013-10-18 22:06:14 +0000

    SSS

---
+++ trunk/SCGESLogicaNegocio/src/main/java/pe/com/ega/sgces/logic/TurnoLogicaImpl.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.com.ega.sgces.logic;
+
+import java.util.Date;
+import org.hibernate.Session;
+import pe.com.ega.sgces.dao.HibernateUtil;
+import pe.com.ega.sgces.dao.InterfaceDaoImpl;
+import pe.com.ega.sgces.dao.TurnoDao;
+import pe.com.ega.sgces.dao.UsuarioDao;
+import pe.com.ega.sgces.model.InterfaceConfig;
+import pe.com.ega.sgces.model.Turno;
+
+/**
+ *
+ * @author Flopez
+ */
+public class TurnoLogicaImpl implements TurnoLogica{
+
+    private InterfaceLogicaImpl interfaceLogica;
+    public TurnoLogicaImpl()
+    {
+        session = HibernateUtil.getSessionFactory().openSession();
+        interfaceLogica= new InterfaceLogicaImpl();
+        interfaceLogica.setInterfaceDao(new InterfaceDaoImpl());
+    }
+    
+    Session session; 
+    TurnoDao turnoDao;
+    
+    
+    @Override
+    public void insertar(Turno turno) {
+        session.beginTransaction();
+        turnoDao.insertar(turno);
+        session.getTransaction().commit();
+        this.cem44();
+        
+    }
+
+    @Override
+    public void actualizar(Turno turno) {
+        session.beginTransaction();
+        turnoDao.actualizar(turno);
+        session.getTransaction().commit();
+    }
+
+    @Override
+    public void eliminar(Turno turno) {
+        session.beginTransaction();
+        turnoDao.eliminar(turno);
+        session.getTransaction().commit();
+    }
+
+    @Override
+    public Turno buscarPorCodigo(String estado) {
+        return turnoDao.buscarPorCodigo(estado);
+    }
+    
+    public void setTurnoDao(TurnoDao turnoDao) {
+       this.turnoDao= turnoDao;
+       this.turnoDao.setSession(session);
+    }
+    
+    public void cem44()
+    {
+        InterfaceConfig cierre=interfaceLogica.buscarPorCodigo(1);
+        
+        cierre.setCambioturno(1);
+        cierre.setFechaTotalizadoresElectronicos(new Date());      
+        interfaceLogica.actualizar(cierre);
+        System.out.println("estado"+cierre.getCambioturno());
+             
+    }
+}
