3dfc4b9cd41357fd4ba3cfb69d2eb4d5bc13eec3
screaber@gmail.com <screaber@gmail.com@681f322d-baa2-ebcc-1b8f-986ddf46640d>
2013-11-07 04:51:19 +0000

    Web

--- trunk/SGCESWebControlFlotas/src/main/java/pe/com/ega/sgces/web/transaccionBean.java
+++ trunk/SGCESWebControlFlotas/src/main/java/pe/com/ega/sgces/web/transaccionBean.java
@@ -11,7 +11,11 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
-import pe.com.ega.sgces.dao.TransaccionDaoImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.transaction.annotation.Transactional;
+import pe.com.ega.sgces.logic.ArqueoLogica;
+import pe.com.ega.sgces.logic.TransaccionLogica;
 import pe.com.ega.sgces.logic.TransaccionLogicaImpl;
 import pe.com.ega.sgces.model.Transaccion;
 
@@ -25,14 +29,18 @@ public class transaccionBean implements Serializable{
 
     private Transaccion transaccion;
     private List<Transaccion> transaccions;
-    private TransaccionLogicaImpl transacciondao;
+    private TransaccionLogica transaccionLogica;
+  
+  private ArqueoLogica arqueoLogica;
     @ManagedProperty("#{loginBean}")
     private LoginBean loginBean;
     private Float totalSoles;
     
     public transaccionBean() {
-        transacciondao = new TransaccionLogicaImpl();
-        transacciondao.setTransaccionDao(new TransaccionDaoImpl());
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+                this.transaccionLogica =  context.getBean("transaccionService",
+				TransaccionLogicaImpl.class);    
     }
     
     public LoginBean getLoginBean() {
@@ -55,7 +63,7 @@ public class transaccionBean implements Serializable{
     }
 
     public List<Transaccion> getTransaccions() {       
-        transaccions=transacciondao.buscarListaId(String.valueOf(loginBean.getUsuario().getId()));
+        transaccions=transaccionLogica.buscarListaId(String.valueOf(loginBean.getUsuario().getId()));
         return transaccions;
     }
 
@@ -74,7 +82,7 @@ public class transaccionBean implements Serializable{
      private Float sumaSoles() {
         Double total = 0.0;
         Double nim;
-        transaccions=transacciondao.buscarListaId(String.valueOf(loginBean.getUsuario().getId()));
+        transaccions=transaccionLogica.buscarListaId(String.valueOf(loginBean.getUsuario().getId()));
         for (Transaccion string : transaccions) {
             nim = Double.parseDouble(String.valueOf(string.getMontototal()));
             total = total+nim;
